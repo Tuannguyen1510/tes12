20a8e2b50a99a7e98c2e919660edf83e1105d335
tuan67720@gmail.com <tuan67720@tuan67720gmail.67720gmailcom>
2022-12-04 00:16:31 +0700

    'tes'

---
+++ tes.js
@@ -0,0 +1,10 @@
+document.querySelector("#button").onclick = function (){
+    let name = document.querySelector("#name").value
+
+    console.log(name);
+
+    document.querySelector("#text").innerHTML= "1234"
+    
+    // text.innerHTML = name;
+}
+// document.querySelector("#text").innerHTML= "1234"
